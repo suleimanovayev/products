119ef80985afe043621b6c47207fd15d1b10b261
suleimanovayev <suleimanovayev@gmail.com>
2021-02-16 21:06:08 +0200

    delete imports

--- src/main/java/com/internet/shop/products/entity/User.java
+++ src/main/java/com/internet/shop/products/entity/User.java
@@ -2,17 +2,12 @@ package com.internet.shop.products.entity;
 
 import lombok.Data;
 import lombok.EqualsAndHashCode;
-import lombok.Getter;
-import lombok.Setter;
-import lombok.ToString;
 
-import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.FetchType;
 import javax.persistence.ManyToMany;
 import javax.persistence.Table;
-import java.util.Objects;
 import java.util.Set;
 
 @EqualsAndHashCode(callSuper = true, exclude = "roles")
