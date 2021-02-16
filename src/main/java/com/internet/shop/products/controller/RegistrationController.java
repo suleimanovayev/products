c716bb6f063cfa428dd019ccb7141db2be0aa84d
suleimanovayev <suleimanovayev@gmail.com>
2021-02-16 15:29:51 +0200

    added password confirm

--- src/main/java/com/internet/shop/products/controller/RegistrationController.java
+++ src/main/java/com/internet/shop/products/controller/RegistrationController.java
@@ -9,6 +9,7 @@ import com.internet.shop.products.service.UserService;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import lombok.Setter;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
 import org.springframework.web.context.annotation.RequestScope;
 
@@ -18,8 +19,10 @@ import javax.faces.component.UIComponent;
 import javax.faces.context.FacesContext;
 import javax.faces.validator.ValidatorException;
 import java.io.Serializable;
-import java.util.Collections;
 
+import static java.util.Collections.singleton;
+
+@Slf4j
 @Component
 @RequestScope
 @RequiredArgsConstructor
@@ -27,7 +30,7 @@ import java.util.Collections;
 @Setter
 public class RegistrationController implements Serializable {
     private final UserService userService;
-    private UserMapper userMapper;
+    private final UserMapper userMapper;
     private UserDto userDto;
 
     @PostConstruct
@@ -38,14 +41,16 @@ public class RegistrationController implements Serializable {
     public String save() throws EmailExistException {
         if (!userService.isEmailAlreadyExist(userDto.getEmail())) {
             User user = userMapper.map(userDto);
-            user.setRoles(Collections.singleton(new Role("USER")));
+            user.setRoles(singleton(new Role("USER")));
             userService.save(user);
+            log.info("Successful saved user with email: {}", userDto.getEmail());
             return "/products.jsf";
         }
-        throw new EmailExistException("Email is already taken, please choose another");
+        log.error("User with email: {}, already exist", userDto.getEmail());
+        throw new EmailExistException(String.format("Email %s is already taken", userDto.getEmail()));
     }
 
-    public void validate(FacesContext facesContext, UIComponent uiComponent, Object password) throws ValidatorException {
+    public void isValidPasswordConfirm(FacesContext context, UIComponent component, Object password)  {
         if (!password.equals(userDto.getPasswordConfirm())) {
             FacesMessage message = new FacesMessage("Password confirm is not equal to password");
             message.setSeverity(FacesMessage.SEVERITY_ERROR);
